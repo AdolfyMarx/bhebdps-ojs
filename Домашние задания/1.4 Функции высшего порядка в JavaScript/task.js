81963fa342df390170df1bc3dd4e995042419c34
Marx <relictpriest@gmail.com>
2026-06-08 22:10:27 +0300

    1.4

--- Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
+++ Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
@@ -1,9 +1,54 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash,
+      value: result,
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count += 1;
+    }
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count += 1;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
